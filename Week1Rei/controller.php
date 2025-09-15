b55a0bed68ed6f6e16e4590b37f5352d8246ad9b
R-W-Alie <rwijayaalie@student.ciputra.ac.id>
2025-09-15 14:49:00 +0700

    what the hell am i doing

--- Week1Rei/controller.php
+++ Week1Rei/controller.php
@@ -191,18 +191,12 @@ function getpairs(){
     return $allData;
 }
 
-function deleteookauthor($book_id){
+function deletebookauthor($book_id){
     $conn = my_connectDB();
-    $sql = "UPDATE book SET author_id = NULL WHERE book_id = " . intval($book_id);
+    $sql = "UPDATE book SET author_id = NULL title = NULL WHERE book_id = " . intval($book_id);
     mysqli_query($conn, $sql);
     my_closeDB($conn);
-}
-
-function deleteauthorbook($book_id){
-    $conn = my_connectDB();
-    $sql = "UPDATE book SET title = NULL WHERE book_id = " . intval($book_id);
-    mysqli_query($conn, $sql);
-    my_closeDB($conn);
+    
 }
 
 if (isset($_POST['button_submit'])) {
